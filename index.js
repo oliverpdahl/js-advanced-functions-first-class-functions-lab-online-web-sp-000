9e10c7a46bee4e153f03ff5c7e3861764cbaaccf
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-12 13:56:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const returnFirstTwoDrivers = function(drivers){
 }
 
 const returnLastTwoDrivers = function(driver){
-  return drivers.slice(0, -2)
+  return drivers.slice(-2, 2)
 }
 
 selectingDrivers = [returnLastTwoDrivers, returnFirstTwoDrivers]
